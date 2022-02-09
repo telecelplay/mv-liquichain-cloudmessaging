1c5ff200cb4cf1ec965999599ace55cb4863178e
meveo.admin <no-mail@meveo.com>
2022-02-10 07:24:43 +0800

    Add the cet java source file : TranasctionMessage.javain the module : liquichain-cloudmessaging

---
+++ facets/java/org/meveo/model/customEntities/TranasctionMessage.java
@@ -0,0 +1,73 @@
+package org.meveo.model.customEntities;
+
+import org.meveo.model.CustomEntity;
+import java.util.List;
+import org.meveo.model.persistence.DBStorageType;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+public class TranasctionMessage implements CustomEntity {
+
+    public TranasctionMessage() {
+    }
+
+    public TranasctionMessage(String uuid) {
+        this.uuid = uuid;
+    }
+
+    private String uuid;
+
+    @JsonIgnore()
+    private DBStorageType storages;
+
+    private String body;
+
+    private String title;
+
+    private String userId;
+
+    @Override()
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public DBStorageType getStorages() {
+        return storages;
+    }
+
+    public void setStorages(DBStorageType storages) {
+        this.storages = storages;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    @Override()
+    public String getCetCode() {
+        return "TranasctionMessage";
+    }
+}
